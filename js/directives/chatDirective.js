357d03466f2eff62c02ff698c04dce74feaa1199
balda38 <alduin2012@gmail.com>
2017-12-14 13:37:50 +0300

    more-styles
    + basket
    + navigation
    + header
    + footer
    + main page slider
    + top offers

--- js/directives/chatDirective.js
+++ js/directives/chatDirective.js
@@ -22,16 +22,34 @@ define(function(){
                     "</div>" +
                     "<div id='chat2' class='chat-with-user'>" +
 					    "<ul id='messages' class='clear'>" +
-						    "<li class='admin-message-cloud'>Здравствуйте!</li>" +
+						    "<li class='admin-message-cloud'>Здравствуйте, {{userName}}!</li>" +
 						    "<br><br>" +
 					    "</ul>" +					    
                     "</div>" +
 			    "</div>"+
                 "<textarea ng-keydown='sendMessage($event)' id='userMessage' type='text' class='chat-input' placeholder='Введите ваше сообщение здесь и нажмите Enter...' ></textarea>",
 			scope:{},
-			controller: function($scope, $attrs){		
-				var msg = document.getElementById('userMessage');
-				var ul = document.getElementById('messages');				
+			controller: function ($, $scope, $attrs) {
+			    var msg = document.getElementById('userMessage');
+			    var ul = document.getElementById('messages');
+			    $scope.userName = undefined;
+
+			    var connection = $.connection('SITE HERE');
+			    connection.start().hub.start().done(function(){
+
+			    });
+
+			    connection.client.addMessage = function(message){
+			        var li = document.createElement('li');
+			        var br = document.createElement('br');
+			        br.setAttribute('style', 'clear: both');
+			        li.setAttribute('class', 'admin-message-cloud');
+			        li.appendChild(document.createTextNode(message));
+			        ul.appendChild(li);
+			        ul.appendChild(br);
+			        ul.appendChild(br);
+			    };
+
 				$scope.sendMessage = function(e){
 					if(e.keyCode == 13){						
 					    e.preventDefault();
@@ -44,18 +62,25 @@ define(function(){
 					        ul.appendChild(li);
 					        ul.appendChild(br);
 					        ul.appendChild(br);
+					        connection.server.sendMessage(msg.value);
 					        msg.value = "";
 					    };
 					};
 				};
 
 				$scope.goChat = function () {
-				    if (document.getElementById("userName").value != ""){
+				    if (document.getElementById("userName").value != "") {
+				        $scope.userName = document.getElementById("userName").value;
+				        connection.server.connect($scope.userName);
+
 				        var chat1 = document.getElementById("chat1");
 				        chat1.parentNode.removeChild(chat1);
 				        document.getElementById("chat2").style.opacity = "1";
 				        document.getElementById("userMessage").style.opacity = "1";
-				    }				    
+				    }
+				    else{
+				        window.alert("Пожалуйста, укажите Ваше имя");
+				    }
 				}
 			},
 			link: function (scope, element, attrs) {					
--- js/directives/smartphoneDirective.js
+++ js/directives/smartphoneDirective.js
@@ -6,18 +6,25 @@ define(function(){
 		return {
 			restrict: 'EACM',			
 			template:
-				"<h3 style='text-align: center'>{{name}}</h3>" +
-					"<p align='center'><img ng-src='{{imgPath}}' width='130' height='250'></p>" +
-					'<ul>' +
-						"<li>Процессор: {{processor}}" +
-						"<li>ОЗУ: {{ram}}" +
-						"<li>Внутренняя память: {{memory}}" +
-						"<li>Аккумулятор: {{battery}}" +
-						"<li>Камера: {{camera}}" +
-						"<li>Экран: {{display}}" +
-						"<li>Другое: {{other}}" +
-					"</ul>" +
-				"<p align='center'><button class='buy-button'>Купить<br>{{price}}</button></p>",
+                "<div class=top-offers>" + 
+				    "<h3 style='text-align: center'>{{name}}</h3>" +
+					    "<p align='center'><img ng-src='{{imgPath}}' width='130' height='250'></p>" +
+					    '<ul>' +
+						    "<li>Процессор: {{processor}}" +
+						    "<li>ОЗУ: {{ram}}" +
+						    "<li>Внутренняя память: {{memory}}" +
+						    "<li>Аккумулятор: {{battery}}" +
+						    "<li>Камера: {{camera}}" +
+						    "<li>Экран: {{display}}" +
+						    "<li>Другое: {{other}}" +
+					    "</ul>" +
+                    "<table class='top-offer-buttons'>"+
+                        "<tr>" +
+                            "<td><button class='buy-button'>Купить<br>{{price}}</button></td>" +
+                            "<td><button class='basket-add-button'>В корзину</button></td>" +
+                        "</tr>" +
+                    "</table>"+
+                "</div>",
 			scope:{
 				name: '@',
 				imgPath: '@',
